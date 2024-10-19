c67b67bc09f56d7b37830df52e464a3d245d70c8
cathleen <cathleenqiao@gmail.com>
2024-10-18 20:14:58 -0700

    creating the tls client hello packet data

--- tlsimpl/__main__.py
+++ tlsimpl/__main__.py
@@ -7,87 +7,9 @@ import time
 from tlsimpl.client import TLSSocket
 
 proc = None
-try:
-    openssl_version = subprocess.run(
-        ["openssl", "-version"],
-        stdin=subprocess.DEVNULL,
-        capture_output=True,
-        check=True,
-    )
-    cert_dir = os.path.join(os.path.dirname(__file__), "../certs")
-    if not os.path.isdir(cert_dir):
-        os.mkdir(cert_dir)
-    has_key = os.path.isfile(os.path.join(cert_dir, "server.key")) and os.path.isfile(
-        os.path.join(cert_dir, "server.csr")
-    )
-    has_pem = os.path.isfile(os.path.join(cert_dir, "server.pem"))
-    if not has_key:
-        print("Generating a server key and signing request...")
-        subprocess.run(
-            [
-                "openssl",
-                "req",
-                "-new",
-                "-subj",
-                "/CN=US/",
-                "-newkey",
-                "rsa:4096",
-                "-nodes",
-                "-keyout",
-                "server.key",
-                "-out",
-                "server.csr",
-                "-quiet",
-            ],
-            cwd=cert_dir,
-            check=True,
-            stdin=subprocess.DEVNULL,
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-        )
-    if not has_pem:
-        print("Generating a server certificate...")
-        subprocess.run(
-            [
-                "openssl",
-                "x509",
-                "-req",
-                "-sha256",
-                "-days",
-                "365",
-                "-in",
-                "server.csr",
-                "-signkey",
-                "server.key",
-                "-out",
-                "server.pem",
-            ],
-            cwd=cert_dir,
-            check=True,
-            stdin=subprocess.DEVNULL,
-            stdout=subprocess.DEVNULL,
-            stderr=subprocess.DEVNULL,
-        )
-    print("Starting an openssl server on port 8080...")
-    proc = subprocess.Popen(
-        ["openssl", "s_server", "-port", "8080", "-trace", "-key", "server.key"],
-        cwd=cert_dir,
-        stdin=sys.stdin,
-        stdout=sys.stdout,
-        stderr=sys.stderr,
-    )
-    time.sleep(1)
-
-except FileNotFoundError:
-    print("Couldn't find openssl command, using example.com instead")
-except subprocess.CalledProcessError:
-    print("Error while running openssl, using example.com instead")
 
 try:
-    if proc is None:
-        s = TLSSocket.create_connection(("example.com", 443))
-    else:
-        s = TLSSocket.create_connection(("127.0.0.1", 8080))
+    s = TLSSocket.create_connection(("127.0.0.1", 8080))
     s.close()
 finally:
     if proc is not None:
--- tlsimpl/handshake.py
+++ tlsimpl/handshake.py
@@ -9,6 +9,39 @@ from typing import Any
 
 from tlsimpl import client, cryptoimpl, util
 from tlsimpl.consts import *
+from tlsimpl.util import *
+import os
+
+def extension(publickey):
+
+    # supported groups
+    data = b'\x00\x1d'
+    sup_gr = pack_varlen(data)
+    sup_gr = pack_varlen(sup_gr)
+    sup_gr = b'\x00\x0a' + sup_gr
+
+    # signature algorithms
+    rsa = b'\x08\x04'
+    sig_alg = pack_varlen(rsa)
+    sig_alg = pack_varlen(sig_alg)
+    sig_alg = b'\x00\x0d' + sig_alg
+
+    # supported versions
+    end = b'\x03\x04'
+    sup_ver = pack_varlen(end, 1)
+    sup_ver = pack_varlen(sup_ver)
+    sup_ver = b'\x00\x2b' + sup_ver
+
+    # key share
+    key_sh = pack_varlen(publickey)
+    key_sh = b'\x00\x1d' + key_sh
+    key_sh = pack_varlen(key_sh)
+    key_sh = pack_varlen(key_sh)
+    key_sh = b'\x00\x33' + key_sh
+
+    return sup_gr + sig_alg + sup_ver + key_sh
+
+#print(extension(b'\x02').hex())
 
 
 def send_client_hello(sock, key_exchange_pubkey: bytes) -> None:
@@ -21,6 +54,32 @@ def send_client_hello(sock, key_exchange_pubkey: bytes) -> None:
     """
     packet = []
     # TODO: construct the packet data
+
+    # extensions
+    ext = extension(key_exchange_pubkey)
+    # print(type(ext))
+    ext = pack_varlen(ext)
+
+    # compression methods
+    com = b'\x01\x00'
+
+    # cipher suites
+    cip = b'\x13\x02'
+    cip = pack_varlen(cip)
+
+    # session ID
+    id = b''
+    id = pack_varlen(id, 1)
+
+    # client random
+    random = os.urandom(32)
+
+    # client version
+    vers = b'\x03\x03'
+
+    total = vers + random + id + cip + com + ext
+    packet.append(total)
+
     sock.send_handshake_record(HandshakeType.CLIENT_HELLO, b"".join(packet))
 
 
